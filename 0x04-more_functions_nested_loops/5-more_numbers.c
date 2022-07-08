351a020062ba772c834cbb3cd33aa0923d961e5e
Prosper67 <asemakehinde67@gmail.com>
2022-07-07 19:44:32 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number from 0-14
+ */
+
+void more_numbers(void)
+{
+	char i;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
