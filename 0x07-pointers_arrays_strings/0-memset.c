fa323ba33378be8e13738b5f7389b9851f966687
Prosper67 <asemakehinde67@gmail.com>
2022-07-19 05:15:14 -0700

    Corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,18 +3,21 @@
 /**
  * _memset - fill n bytes of memory with a cosntant value
  * @s: pointer to memory area
- * @b: character to fill the memory with
+ * @b: constant value
  * @n: number of bytes to fill
  * Return:pointer to the filled memory area @s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = b;
+	int size = n;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+	if (size > 0)
+	{
+		int i;
 
-	return (memory);
+		for (i = 0; i < size; i++)
+			*(s + i) = b;
+	}
+	return (s);
 }
