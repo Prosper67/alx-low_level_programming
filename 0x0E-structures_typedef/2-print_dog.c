19df2750d1a14b6f19a5dae0f52ca3a93071f712
Prosper67 <asemakehinde67@gmail.com>
2022-08-02 09:35:52 -0700

    Commit all

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints all member info of a struct dog
+ * @d: a variable of type struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL) /*validate if d initialized correctly*/
+		return;
+
+	(d->name == NULL) ? printf("Name: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
+}
