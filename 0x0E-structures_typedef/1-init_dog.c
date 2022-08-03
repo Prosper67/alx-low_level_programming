bec8fb9462e964c3d0630dd3b02675a90dec62ea
Prosper67 <asemakehinde67@gmail.com>
2022-08-03 13:54:51 -0700

    1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL) /* validate if d initialized correctly*/
-		return;
+	if (d != NULL) /* validate if d initialized correctly*/
+	{
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
